192e35f31c71160ed7d602e74d6306f2fd6915d4
Synyster991 <f_dimitrievski@outlook.com>
2020-01-06 19:30:49 -0500

    Account URLs, Account templates and links on navbar

--- account/views.py
+++ account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    return render(request, 'account/login.html')
+
+
+def signup(request):
+    return render(request, 'account/signup.html')
+
+
+def logout(request):
+    return render(request, 'account/signup.html')

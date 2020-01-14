4a5690be93c17fe2e0e16131fb95706ccad117cd
Synyster991 <f_dimitrievski@outlook.com>
2020-01-13 19:40:39 -0500

    Product Model Created

--- product/models.py
+++ product/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    pubDate = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votesTotal = models.IntegerField(default=1)
+    hunter =  models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pubDatePretty(self):
+        return self.pubDate.strftimr('%b %e %Y')
